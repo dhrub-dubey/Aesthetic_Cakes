f26dd19beba51ccd56761978319a05c69c9b99c8
Watson-Dhrub <dhrubdubey5997@gmail.com>
2025-07-09 21:24:51 +0530

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Aesthetic_Cakes/',
+  base: '/DhrubDubey_LetsUpgradeFrontendSprint/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
